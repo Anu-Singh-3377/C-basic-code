e29845e6e7210f4040b6cdd4077a1f98bd8ce6d7
Anu-Singh-3377 <08anushkasingh11e@gmail.com>
2025-08-23 08:42:33 +0530

    second

---
+++ agecondition.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int age;
+    cin>>age;
+    if(age>=18){
+        cout<< "YOU ARE A ADULT";
+    }
+    else{
+        cout<< "YOU ARE NOT ADULT";
+    }
+    
+    return 0;
+}
---
+++ stringfile.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s= "HELLO";
+    cout<< s[2];
+    /* lenth of the string */
+    int len=s.size();
+    cout<<"size of the sgtring  "<< len;
+    return 0;
+}
