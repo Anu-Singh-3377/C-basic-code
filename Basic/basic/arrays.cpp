09a305ccdfd96b786598d26edbdcb5bdf2ad6781
Anu-Singh-3377 <08anushkasingh11e@gmail.com>
2025-08-29 19:44:43 +0530

    Add files via upload

---
+++ Basic/basic/arrays.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    /* one-dimention araay
+     
+    int arr[5];
+    cin>> arr[0] >> arr[1] >> arr[2]>> arr[3]>> arr[4];
+    cout<< arr[3];
+    arr[2]+=10;
+    cout<<arr[2];
+    return 0;
+
+    */
+    /*2-d aaray*/
+    int arr[3][5];
+    arr[1][3] = 78;
+    cout<< arr[1][3];
+    return 0;
+}
